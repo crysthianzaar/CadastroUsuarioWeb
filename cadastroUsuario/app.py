14e49b1e5a95e6579b0ee24321a1c86fdf849fe1
Crysthian <crysthianmx@hotmail.com>
2021-12-28 00:18:30 -0300

    app to application

--- cadastroUsuario/app.py
+++ cadastroUsuario/app.py
@@ -3,11 +3,11 @@ from cadastroUsuario.extensions import configuration, database, commands, sessio
 from cadastroUsuario.blueprints import restapi, webui
 
 def create_app():
-    app = Flask(__name__)
-    configuration.init_app(app)         # Gerenciamento de Ambientes e Configurações
-    commands.init_app(app)              # Comandos de Sistema
-    database.init_app(app)              # Configuração do Banco de Dados
-    session_management.init_app(app)    # Gerenciamento de Sessão de Usuário
-    restapi.init_app(app)               # API REST para gerenciamento de usuários
-    webui.init_app(app)                 # Templates, Login e Cadastro de Usuário
-    return app
+    application = Flask(__name__)
+    configuration.init_app(application)         # Gerenciamento de Ambientes e Configurações
+    commands.init_app(application)              # Comandos de Sistema
+    database.init_app(application)              # Configuração do Banco de Dados
+    session_management.init_app(application)    # Gerenciamento de Sessão de Usuário
+    restapi.init_app(application)               # API REST para gerenciamento de usuários
+    webui.init_app(application)                 # Templates, Login e Cadastro de Usuário
+    return application
